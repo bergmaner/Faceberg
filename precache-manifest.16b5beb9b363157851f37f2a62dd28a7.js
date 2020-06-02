a3ebba48fbf69a254370cfe6149a8d15a76f3b0e
bergmaner <bergkacper@gmail.com>
2020-06-02 16:47:46 +0200

    Updates

---
+++ precache-manifest.16b5beb9b363157851f37f2a62dd28a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0269fd0f9e41479d5ebdb08bcea2248b",
+    "url": "/faceberg/index.html"
+  },
+  {
+    "revision": "030782be9bf4aa4bfc4b",
+    "url": "/faceberg/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ed3e34413430e752398",
+    "url": "/faceberg/static/js/2.962311aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceberg/static/js/2.962311aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030782be9bf4aa4bfc4b",
+    "url": "/faceberg/static/js/main.9da74f6e.chunk.js"
+  },
+  {
+    "revision": "6724f3c1381aa0230f36",
+    "url": "/faceberg/static/js/runtime-main.d2a4cedc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/faceberg/static/media/logo.5d5d9eef.svg"
+  }
+]);
